ca48a7541c7cce7fa2e122ff3659a1e90a742148
Hector Diaz <hectordiaz1103@gmail.com>
2021-01-13 15:36:04 -0500

    added date to the top of the page and changed temperature to fahrenheit

--- weather-app.js
+++ weather-app.js
@@ -4,45 +4,55 @@ $(document).ready(function() {
 var cities = [];
 
 //capture city name
-// need explanation on this
 function dispayCityWeather() {
 
+    //would like to clear the screen before showing new information
+
+    // need explanation on this
     var city = $(this).attr("data-name");
     console.log(city);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=697b47836ddecca53fd9822ab4c82a6c"
+
+    // var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=697b47836ddecca53fd9822ab4c82a6c"
+    
+    var currentDate = new Date();
+    var month = currentDate.getMonth()+1;
+    var day = currentDate.getDate();
+    var year = currentDate.getFullYear();
+    var fullDate = month + "/" + day + "/" + year;
+    $("#date").text(fullDate);
 
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-
         var todayDiv = $("<div class = todayWeather>");
-
         var cityName = response.name;
         var one = $("<h5>").text("Weather for " + cityName + ":");
         console.log(one);
         todayDiv.append(one);
-
-        var weather = response.main.temp;
-        var two = $("<p>").text("The temp today is: " + weather);
+        var weather = (response.main.temp - 273.15) * 1.80 + 32;;
+        var two = $("<p>").text("The temp today is: " + weather.toFixed(2));
         todayDiv.append(two);
-
-        var feels = response.main.feels_like;
-        var three = $("<p>").text("But it feels like: " + feels);
+        var feels = (response.main.feels_like - 273.15) * 1.80 + 32;;
+        var three = $("<p>").text("But it feels like: " + feels.toFixed(2));
         todayDiv.append(three);
-
-        var low = response.main.temp_min;
-        var four = $("<p>").text("The low for today is: " + low);
+        var low = (response.main.temp_min - 273.15) * 1.80 + 32;;
+        var four = $("<p>").text("The low for today is: " + low.toFixed(2));
         todayDiv.append(four);
-
-        var high = response.main.temp_max;
-        var five = $("<p>").text("The high will be: " + high);
+        var high = (response.main.temp_max - 273.15) * 1.80 + 32;;
+        var five = $("<p>").text("The high will be: " + high.toFixed(2));
         todayDiv.append(five);
-
+        var des = response.weather[0].main;
+        //adding icon
+        var icon = response.weather[0].icon;
+        var showIcon = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        var six = $("<p>").text("Current condition: " + des + " " + showIcon);
+        todayDiv.append(six);
         $("#today").prepend(todayDiv);
-
       });
 }
+
 
 //function to display weather
 function addButtons() {
